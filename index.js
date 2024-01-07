16d98b34995fbdb4745d6a57cc37000dca0612a8
John Henckel <jdhenckel@gmail.com>
2024-01-06 20:53:51 -0600

    fixes

--- index.js
+++ index.js
@@ -8,6 +8,36 @@
 //
 //  For example state = ['1,2', 'R', 'GBR', 'BBB', 'GBGG'] is four tubes
 
+class State {
+    constructor (tubes, m=[], s=0) {
+        this.tubes = tubes;
+        this.moves = m;
+        this.score = s;
+    }
+
+    not_solved(m=0) {
+        let n = 0;
+        for (let t of this.tubes)
+            if (top_length(t) < Math.max(m,t.length)) ++n;
+        return n;    
+    }
+
+
+    num_empty() {
+        let n = 0;
+        for (let t of this.tubes)
+            if (t.length == 0) ++n;
+        return n;    
+    }
+
+    compute_score(m=0) {
+        let n = this.tubes.length;
+        let done = n - this.not_solved(m);
+        let pure = n - this.not_solved();
+        let empty = this.num_empty();
+        this.score = done + pure + empty;
+    }
+}
 
 // Return the length of the top color, e.g. 'AAB' returns 2.
 function top_length(tube) {
@@ -20,7 +50,7 @@ function top_length(tube) {
 function pour(a,b,m) {
     if (a.length > 0 && (b.length==0 || a[0] == b[0])) {
         let i = top_length(a);
-        if (i < a.length && i + b.length <= m) 
+        if (i < m && i + b.length <= m) 
             return [a.slice(i),a.slice(0,i)+b];
     }
     return [];
@@ -29,19 +59,24 @@ function pour(a,b,m) {
 // Given a state, find all descendents, i.e. all states that are the result of a valid action.
 // If no actions are valid, return empty list.  m is the tube size.
 function get_choices(state, m=4) {
-    let n = state.length;
-    let s = state
+    let tub = state.tubes
+    let n = tub.length;
     let result = [];
-    for (let i=1; i<n; ++i) {
-        for (let j=i+1; j<n; ++j) if (i!=j) {
-            let [a,b] = pour(s[i], s[j], m);
-            if (a) {
-                let t  = [s[0]+','+i+'-'+j].concat(s.slice(1,i), a, s.slice(i+1,j), b, s.slice(j+1));
+    //let ei = 0;
+    for (let i=0; i<n; ++i) {
+        let ej = 0
+        for (let j=i+1; j<n; ++j) {
+            let [a1,b1] = pour(tub[i], tub[j], m);
+            if (a1!=null && (tub[j].length || !ej)) {
+                let t = new State(tub.slice(0,i).concat(a1, tub.slice(i+1,j), b1, tub.slice(j+1)),
+                    state.moves.concat(i+'-'+j));
                 result.push(t);
+                if (!tub[j].length) ej = 1;
             }
-            [b,a] = pour(s[j], s[i], m);
-            if (a) {
-                let t  = [s[0]+','+j+'-'+i].concat(s.slice(1,i), a, s.slice(i+1,j), b, s.slice(j+1));
+            let [b2,a2] = pour(tub[j], tub[i], m);
+            if (a2!=null && (a1 || b2)) {
+                let t = new State(tub.slice(0,i).concat(a2, tub.slice(i+1,j), b2, tub.slice(j+1)),
+                    state.moves.concat(j+'-'+i));
                 result.push(t);
             }
         }
@@ -49,53 +84,58 @@ function get_choices(state, m=4) {
     return result;
 }
 
-
-// Return the number of tubes that are NOT solved in the state
-function not_solved(state) {
-    let n = 0;
-    for (let i=1; i<state.length; ++i) {
-        if (top_length(state[i]) < state[i].length) ++n;
+// INsert states into the todo, keep them ordered DESCENDING by score
+function add_states(todo, states, m=0) {
+    for (let s of states) {
+        s.compute_score(m);
+        for (var i=0; i<todo.length && todo[i].score > s.score; ) ++i;
+        todo.splice(i,0,s);
     }
-    return n;
 }
 
-
-function test_choices() {
-    let a = ['','ABC','AA','ACC','BBC','AAAA'];
-    console.log('=======================');
-    console.log(a);
-    console.log('=======================\nfrom:',a);
-    a = get_choices(a);
-    for (let x of a) console.log(x);
-    console.log('=======================\nfrom:',a[0]);
-    a = get_choices(a[0]);
-    for (let x of a) console.log(x);
-    console.log('=======================\nfrom:',a[0]);
-    a = get_choices(a[0]);
-    for (let x of a) console.log(x);
-    console.log('=======================\nfrom:',a[0]);
-    a = get_choices(a[0]);
-    for (let x of a) console.log(x);
-}
 
 // Return a sequence of actions that will solve the water sort.  
 function solve(state, m=4) {
     let todo = [state];
     while (todo.length > 0) {
-        var a = todo.shift();         // BREADTH FIRST SEARCH
-        if (not_solved(a) == 0) break;
+        var a = todo.shift();         // BEST FIRST SEARCH
+        if (a.not_solved(m) == 0) break;
         let b = get_choices(a, m);
-        todo = todo.concat(b);
-        // console.log('Todo len = ',todo.length);
+        add_states(todo, b, m);
+        console.log(todo.length,todo[0].moves.length);
     }
-    return a;//a[0].substring(1);
+    return a;
 }
 
-function test_solver(i) {
-    let a = ['','ABC','AA','ACC','BBC','AAAA'];
-    if (i==2) 
-        a = ['','rgpb','lggo','roGp','lbrg','lGpl','Gbop','obrG','',''];  // #13
-    console.log('=========\nTry to solve: ', a);
-    let b = solve(a);
-    console.log('Solved: ',b);
+
+
+function test_choices() {
+    let a = new State(['ABC','AA','ACC','BBC','B']);
+    console.log('=======================');
+    console.log(a.moves,a.tubes);
+    console.log('=======================\nfrom:',a.moves,a.tubes);
+    a = get_choices(a);
+    for (let x of a) console.log(x.moves,x.tubes);
+    for (let y=0; y<20 && a.length; ++y) {
+        console.log('=======================\nfrom:',a[0].moves,a[0].tubes);
+        a = get_choices(a[0]);
+        for (let x of a) console.log(x.moves,x.tubes);
+    }
 }
+
+
+function test_solver(i) {
+    let a = ['ABC','AA','ACC','BBC','B'];
+    if (i==13) 
+        a = ['rgpb','lggo','roGp','lbrg','lGpl','Gbop','obrG','',''];  // #13
+    if (i==37) 
+        a = ['abgG','ggpG','PrGo','apll','lrba','robr','GlpP','aooP','bpPg','','']; 
+    console.log('=========\nTry to solve: ', a);
+    let b = solve(new State(a));
+    console.log('Solved: ',b.moves,b.tubes);
+}
+
+test_solver(13);
+//test_choices();
+
+
